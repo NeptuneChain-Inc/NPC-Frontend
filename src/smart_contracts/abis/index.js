79b48ddd32053cdee06ae2e4e719301bb7495c13
amakodev <amakodev@gmail.com>
2024-09-20 00:06:00 +0200

    old version cleanup

--- src/smart_contracts/abis/index.js
+++
@@ -1,9 +0,0 @@
-import MarketplaceContract  from "./marketplace_contract";
-import VerificationContract from "./verification_contract";
-import * as ContractStandards from "./standards"
-
-export {
-    MarketplaceContract,
-    VerificationContract,
-    ContractStandards
-}
--- src/smart_contracts/interactions/index.js
+++
@@ -1,7 +0,0 @@
-import getVerificationInteractions from "./verification";
-import getMarketInteractions from "./marketplace";
-
-export {
-    getVerificationInteractions,
-    getMarketInteractions
-}
